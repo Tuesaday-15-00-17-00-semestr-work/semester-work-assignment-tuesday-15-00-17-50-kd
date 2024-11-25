0aaed34ab41864366fa9b351563a62a62d93d47f
bigblueworm <xvynnykk@stuba.sk>
2024-11-25 21:27:53 +0100

    Add book entity.

---
+++ SpringBootApi/src/main/java/com/virtuallibrary/springbootapi/api/model/Book.java
@@ -0,0 +1,58 @@
+package com.virtuallibrary.springbootapi.api.model;
+
+public class Book {
+
+    private int bookId;
+    private String title;
+    private String author;
+    private String isbnNumber;
+    private int availableCopies;
+
+    public Book(int bookId, String title, String author, String isbnNumber, int availableCopies) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.isbnNumber = isbnNumber;
+        this.availableCopies = availableCopies;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbnNumber() {
+        return isbnNumber;
+    }
+
+    public void setIsbnNumber(String isbnNumber) {
+        this.isbnNumber = isbnNumber;
+    }
+
+    public int getAvailableCopies() {
+        return availableCopies;
+    }
+
+    public void setAvailableCopies(int availableCopies) {
+        this.availableCopies = availableCopies;
+    }
+}
