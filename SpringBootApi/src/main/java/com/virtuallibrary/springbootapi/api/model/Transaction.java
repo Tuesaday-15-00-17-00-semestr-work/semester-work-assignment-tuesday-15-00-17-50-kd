1060ad4a261fd44572582c99b0c4d572521bbd09
bigblueworm <xvynnykk@stuba.sk>
2024-11-30 13:20:16 +0100

    Add transaction class.

---
+++ SpringBootApi/src/main/java/com/virtuallibrary/springbootapi/api/model/Transaction.java
@@ -0,0 +1,75 @@
+package com.virtuallibrary.springbootapi.api.model;
+
+public class Transaction {
+    private int transactionId;
+    private int userId;
+    private int bookId;
+    private String action;
+    private String date;
+
+    public Transaction(int transactionId, int userId, int bookId, String action, String date) {
+        this.transactionId = transactionId;
+        this.userId = userId;
+        this.bookId = bookId;
+        this.action = action;
+        this.date = date;
+    }
+
+    public Transaction(int userId, int bookId, String action, String date) {
+        this.userId = userId;
+        this.bookId = bookId;
+        this.action = action;
+        this.date = date;
+    }
+
+    public int getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(int transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "transactionId=" + transactionId +
+                ", userId=" + userId +
+                ", bookId=" + bookId +
+                ", action='" + action + '\'' +
+                ", date='" + date + '\'' +
+                '}';
+    }
+}
