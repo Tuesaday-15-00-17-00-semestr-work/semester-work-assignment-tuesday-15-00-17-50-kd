7ef87dbb2d1d9796caa0a7c4c2cf0df8415a095a
bigblueworm <xvynnykk@stuba.sk>
2024-12-04 01:00:49 +0100

    Add ViewChanger model

---
+++ JavaFXApp/src/main/java/com/virtuallibrary/javafxapp/Models/ViewChanger.java
@@ -0,0 +1,36 @@
+package com.virtuallibrary.javafxapp.Models;
+
+import com.virtuallibrary.javafxapp.LibraryApplication;
+import javafx.animation.PauseTransition;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class ViewChanger {
+
+    public ViewChanger() {
+
+    }
+
+    public void switchScenes(AnchorPane currentAnchorPane, String newSceneFxmlPath) throws IOException {
+        FXMLLoader loader = new FXMLLoader(Objects.requireNonNull(LibraryApplication.class.getResource(newSceneFxmlPath)));
+        AnchorPane newAnchorPane = loader.load();
+        currentAnchorPane.getChildren().removeAll();
+        currentAnchorPane.getChildren().setAll(newAnchorPane);
+    }
+
+    public void switchSceneWithDelay(AnchorPane currentAnchorPane, String scenePath, int delaySeconds) {
+        PauseTransition pause = new PauseTransition(Duration.seconds(delaySeconds));
+        pause.setOnFinished(event -> {
+            try {
+                switchScenes(currentAnchorPane, scenePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        pause.play();
+    }
+}
