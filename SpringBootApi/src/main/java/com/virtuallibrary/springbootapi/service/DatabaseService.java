24e1b76d3f0fd72c3cd3ff059bf67e6e5a85dfa6
Daryna44 <xvynnyk@stuba.sk>
2024-12-06 16:44:18 +0100

    Small changes

--- SpringBootApi/src/main/java/com/virtuallibrary/springbootapi/service/DatabaseService.java
+++ SpringBootApi/src/main/java/com/virtuallibrary/springbootapi/service/DatabaseService.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DatabaseService {
-    private final String jdbcUrl = "jdbc:sqlite:/C:\\Users\\Admin\\JavaProjects\\virtuallibrary.db";
+    private final String jdbcUrl = "jdbc:sqlite:/C:\\Users\\Admin\\Desktop\\virtuallibrary.db";
     private Connection connection;
     private Statement statement;
 
